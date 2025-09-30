e72c62720b5426ef1616ddd23e5589a70d6fb95c
luannahrens <lahrens@gmail.com>
2025-09-30 10:17:45 -0700

    created functions getHumanChoice, getComputerChoice and playRound

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ Project form The Ordin Project Foundations Course
 Assignment: 
 For this project, you will create the game Rock Paper Scissors. This game will be played entirely in the console.
 
+CALL playGame
+    CALL playRound 
+        CALL getComputerChoice will return one of the following string values: “rock”, “paper” or “scissors”.
+        CALL getHumanChoice 
+        IF human choice and computer choice equal THEN tie
+        ELSE IF 
+        human choice equals "rock" AND computer choice equals "scissors" OR
+        human choice equals "paper" AND comupter choice equals "rock"  OR
+        human choice equals "scissors" AND computer choice equals "paper" 
+        THEN human wins
+        ELSE computer wins
+        update scores   
+    END playRound
+END playGame
+
+
--- javascript.js
+++ javascript.js
@@ -1,1 +1,49 @@
 console.log ("Hello from script file");
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+function getHumanChoice (){
+    let choice = prompt("rock, paper, or scissors");
+    //console.log("Human choice: " + choice.toLowerCase());
+    return choice.toLowerCase();   
+}
+
+function getComputerChoice () {
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * 3)];
+}
+
+function playRound(humanChoice, computerChoice){
+    
+    console.log("Human choice: " + humanChoice);
+    console.log("Computer choice: " + computerChoice);
+    
+    if (humanChoice == computerChoice) {
+        console.log("It's a tie.");
+    }
+    else if ((humanChoice == "rock" && computerChoice == "scissors") || 
+             (humanChoice == "paper" && computerChoice == "rock")   ||
+             (humanChoice == "scissors" && computerChoice == "paper") 
+            ) 
+    { 
+        console.log ("You WIN! " + humanChoice + " beats " + computerChoice);
+        humanScore ++; 
+    }
+    else { 
+        console.log ("You LOSE. " + computerChoice + " beats " + humanChoice);
+        computerScore ++; 
+    }
+
+}
+
+playRound(getHumanChoice(), getComputerChoice());
+
+
+
+
+
+
+
