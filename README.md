0e2dbabf00ce844832ef2c3fbcc7e382da46edcf
luannahrens <lahrens@gmail.com>
2025-09-30 11:25:29 -0700

    added playGame function to keep track of scores during rounds and detrmine overall winner of game

--- README.md
+++ README.md
@@ -2,19 +2,23 @@ Project form The Ordin Project Foundations Course
 Assignment: 
 For this project, you will create the game Rock Paper Scissors. This game will be played entirely in the console.
 
+Pseudocode for Rock, paper, scissors game:
 CALL playGame
-    CALL playRound 
-        CALL getComputerChoice will return one of the following string values: “rock”, “paper” or “scissors”.
-        CALL getHumanChoice 
-        IF human choice and computer choice equal THEN tie
-        ELSE IF 
-        human choice equals "rock" AND computer choice equals "scissors" OR
-        human choice equals "paper" AND comupter choice equals "rock"  OR
-        human choice equals "scissors" AND computer choice equals "paper" 
-        THEN human wins
-        ELSE computer wins
-        update scores   
-    END playRound
+    FOR EACH round
+        CALL playRound 
+            CALL getComputerChoice will return one of the following string values: “rock”, “paper” or “scissors”.
+            CALL getHumanChoice 
+            IF human choice and computer choice equal THEN tie
+            ELSE IF 
+            human choice equals "rock" AND computer choice equals "scissors" OR
+            human choice equals "paper" AND comupter choice equals "rock"  OR
+            human choice equals "scissors" AND computer choice equals "paper" 
+            THEN human wins
+            ELSE computer wins   
+        END playRound
+    update overall scores
+    END FOR 
+    determine overall winner
 END playGame
 
 
--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
-console.log ("Hello from script file");
+// javascript for rock, paper, scissors game for The Odin Project
 
-let humanScore = 0;
-let computerScore = 0;
+const human = "HUMAN";
+const computer = "COMPUTER";
+
 
 
 
@@ -30,16 +31,49 @@ function playRound(humanChoice, computerChoice){
             ) 
     { 
         console.log ("You WIN! " + humanChoice + " beats " + computerChoice);
-        humanScore ++; 
+        return human;
     }
     else { 
         console.log ("You LOSE. " + computerChoice + " beats " + humanChoice);
-        computerScore ++; 
+        return computer;
     }
 
 }
 
-playRound(getHumanChoice(), getComputerChoice());
+function playGame(rounds){
+    
+    let totalRounds = rounds;
+    let humanScore = 0;
+    let computerScore = 0;
+
+    //determine winner
+    let roundWinner = "";
+
+    while (rounds > 0) {
+        roundWinner = playRound(getHumanChoice(), getComputerChoice());
+        if ( roundWinner == human ) {
+            humanScore ++; 
+        } else {
+            computerScore ++;
+        }
+        rounds--;
+    }
+    
+    //code to determine overall winner here
+    if (humanScore == computerScore) {
+        console.log ("It's a tie overall.")
+    } else if (humanScore > computerScore){
+        console.log ("YOU are the overall WINNER!! You won " + humanScore + " of " + totalRounds + "!");
+    } else {
+        console.log("You LOST overall with only " + humanScore + " of " + totalRounds + ".");
+    }
+    
+
+}
+
+let number = 5;
+playGame(number); //argument is # of rounds
+//playRound(getHumanChoice(), getComputerChoice());
 
 
 
